31a18bdb8847c3131f2b41e29610838d99f824c6
Grant Elliott <grant@anticub.us>
2019-01-02 20:22:58 -0500

    Adds yargs for better argument processing and enables more than 2 files to be merged.

--- index.js
+++ index.js
@@ -1,64 +1,55 @@
 'use strict'
 let util = require('util');
+let yargs = require('yargs');
 
 let merge = require('./merge.js');
 let jsonHelper = require('./json.helper.js');
 
-let args = process.argv.slice(2);
+let mergeMethod = yargs.argv.m || 'shallow';
+let mergeType = yargs.argv.t || 'spread';
+let outputFile = yargs.argv.o;
+let inputFiles = yargs.argv._;
 
-doMerge(args).then((result) => {
-  jsonHelper.writeJson(result, './output/merged.json');
+
+doMerge(mergeMethod, mergeType, inputFiles).then((result) => {
+  if (outputFile) {
+    jsonHelper.writeJson(result, outputFile);
+  }
+
   console.log(util.inspect(result, { depth: null }));
 }).catch((err) =>  {
   console.error(err);
 });
 
-async function doMerge(args) {
-  let object1 = await jsonHelper.openJson(args[args.length - 2]);
-  let object2 = await jsonHelper.openJson(args[args.length - 1]);
+async function doMerge(mergeMethod, mergeType, inputFiles) {
+  let inputJsonPromises = inputFiles.map((fileName) => {
+    return jsonHelper.openJson(fileName);
+  });
+
+  let objects = await Promise.all(inputJsonPromises);
 
   return new Promise((resolve, reject) => {
-    let shallowCallback = (err, result) => {
-      if (err) {
-        return reject(err);
-      }
-  
-      return resolve(result);
-    };
+    var result = {};
 
-    let deepCallback = (result) => {
-      return resolve(result);
-    }
-    
-    if (args.length === 2) {
-      // Default to a shallow merge using the spread operator
-      merge.shallow(object1, object2, shallowCallback);
-    } else if (args.length === 3) {
-      // Merge two files with a merge type specified.
-      let mergeType = args[0].toString().toLowerCase();
+    if (mergeMethod === 'deep') {
+      objects.forEach((obj) => {
+        merge.deep(result, obj, (mergeResult) => {
+          result = mergeResult;
+        });
+      });
+    } else {
+      objects.forEach((obj) => {
+        merge.shallow(result, obj, mergeType, (err, mergeResult) => {
+          if (err) {
+            reject(err);
+          }
 
-      if (mergeType === 'shallow') {
-        // Shallow merge with default spread behavior
-        merge.shallow(object1, object2, shallowCallback);
-      } else if (mergeType === 'deep') {
-        merge.deep(object1, object2, deepCallback);
-      } else {
-        reject('The first parameter must be the merge type of either "shallow" or "deep".');
-      }
-
-    } else if (args.length === 4) {
-      // Merge two files with a shallow merge using the specified merge method.
-      let mergeType = args[0].toString().toLowerCase();
-      let mergeMethod = args[1].toString().toLowerCase();
-
-      if (mergeType === 'shallow') {
-        merge.shallow(object1, object2, mergeMethod, shallowCallback);
-      } else {
-        reject('Only a shallow merge can have a merge method parameter.');
-      }
+          result = mergeResult;
+        });
+      });
     }
 
-    reject('Too many arguments supplied. This merge utility isn\'t perfect!');
+    resolve(result);
   });
 }
 
--- readme.md
+++ readme.md
@@ -7,22 +7,23 @@ To install the dev environment perform `npm install` from the package root.
 To run tests execute `npm test`. Tests are written using Jasmine and are stored under the `spec` directory.
 
 ## Use (CLI)
-To merge using the default shallow merge:
-- `node index.js pathToJsonOne pathToJsonTwo`
-- `node index.js shallow pathToJsonOne pathToJsonTwo`
+To merge an option using the defaults:
+`node index.js pathToJsonOne pathToJsonTwo pathToJsonThree ...`
 
-To merge using a shallow merge and a specified merge method:
-* Spread: `node index.js shallow spread pathToJsonOne pathToJsonTwo`
-* Assign: `node index.js shallow assign pathToJsonOne pathToJsonTwo`
-* Assign: `node index.js shallow loop pathToJsonOne pathToJsonTwo`
+There are two options to merge, shallow and deep. The default is a shallow merge. The merge method can be specified with the `-m` flag:
+* Shallow: `node index.js -m shallow pathToJsonOne pathToJsonTwo pathToJsonThree ...`
+* Deep: `node index.js -m deep pathToJsonOne pathToJsonTwo pathToJsonThree ...`
 
-To perform a deep merge:
-- `node index.js deep pathToJsonOne pathToJsonTwo`
+The shallow merge has multiple merge types available behind the `-t` flag:
+* Spread: `node index.js -m shallow -t spread pathToJsonOne pathToJsonTwo`
+* Assign: `node index.js -m shallow -t assign pathToJsonOne pathToJsonTwo`
+* Loop: `node index.js -m shallow -t loop pathToJsonOne pathToJsonTwo`
 
-The output is stored to a file at `output/merged.json` and printed to the console. The printed merge result may be piped into other utilities as needed.
+You can specify an output file using the `-o` flag:
+`node index.js -o ./output/merged.json pathToJsonOne pathToJsonTwo pathToJsonThree ...`
 
 ## Sample Files
-Two sample JSONs are provided in the `spec/data` directory. Perform merges using the utility with:
-- `node index.js ./spec/data/fileOne.json ./spec/data/fileTwo.json`
-- `node index.js shallow ./spec/data/fileOne.json ./spec/data/fileTwo.json`
-- `node index.js deep ./spec/data/fileOne.json ./spec/data/fileTwo.json`
+Three sample JSONs are provided in the `spec/data` directory. Perform merges using the utility with:
+- `node index.js ./spec/data/fileOne.json ./spec/data/fileTwo.json ./spec/data/fileThree.json`
+- `node index.js -m shallow ./spec/data/fileOne.json ./spec/data/fileTwo.json ./spec/data/fileThree.json`
+- `node index.js -m deep ./spec/data/fileOne.json ./spec/data/fileTwo.json ./spec/data/fileThree.json`
